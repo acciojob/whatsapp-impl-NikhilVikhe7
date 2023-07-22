7e7d75410c1408ce166f8d0d511e91e90d903b4f
NikhilVikhe7 <nikhikvikhe7@gmail.com>
2023-07-22 18:36:27 +0530

    Attempt 2

--- src/main/java/com/driver/User.java
+++ src/main/java/com/driver/User.java
@@ -6,6 +6,11 @@ public class User {
     private String name;
     private String mobile;
 
+    public User(String name, String mobile) {
+        this.name = name;
+        this.mobile = mobile;
+    }
+
     public String getName() {
         return name;
     }
