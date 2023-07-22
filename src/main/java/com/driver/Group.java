10a9ce0ec632771bb0fba41a01b8216ebf5a82f8
NikhilVikhe7 <nikhikvikhe7@gmail.com>
2023-07-22 18:48:10 +0530

    Attempt 5

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -29,17 +29,5 @@ public class Group {
     public void setNumberOfParticipants(int numberOfParticipants) {
         this.numberOfParticipants = numberOfParticipants;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Group group = (Group) o;
-        return numberOfParticipants == group.numberOfParticipants && name.equals(group.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, numberOfParticipants);
-    }
+    
 }
